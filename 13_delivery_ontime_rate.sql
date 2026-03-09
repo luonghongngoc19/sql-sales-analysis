c013de84fa1776dbbd53ca40fa7bf1a5a35709b9
Luong Hong Ngoc <ngocleo2001@gmail.com>
2026-03-09 14:58:50 +0700

    Delete 13_delivery_ontime_rate.sql

--- 13_delivery_ontime_rate.sql
+++
@@ -1,9 +0,0 @@
-SELECT YEAR(OrderDate) AS [Year],
-       COUNT(*) AS OrdersShipped,
-       SUM(CASE WHEN ShipDate <= DueDate THEN 1 ELSE 0 END) AS OnTimeOrders,
-       SUM(CASE WHEN ShipDate >  DueDate THEN 1 ELSE 0 END) AS LateOrders,
-       SUM(CASE WHEN ShipDate <= DueDate THEN 1 ELSE 0 END)*1.0 / NULLIF(COUNT(*),0) AS OnTimeRate
-FROM Sales.SalesOrderHeader
-WHERE ShipDate IS NOT NULL
-GROUP BY YEAR(OrderDate)
-ORDER BY [Year];
