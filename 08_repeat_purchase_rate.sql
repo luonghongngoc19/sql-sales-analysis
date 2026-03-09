efaf3b6f5d2b4b7080060ed0724e090c9494b16f
Luong Hong Ngoc <ngocleo2001@gmail.com>
2026-03-09 14:58:35 +0700

    Delete 08_repeat_purchase_rate.sql

--- 08_repeat_purchase_rate.sql
+++
@@ -1,7 +0,0 @@
-WITH x AS (
-	SELECT CustomerID, COUNT(DISTINCT SalesOrderID) AS Orders
-	FROM Sales.SalesOrderHeader
-	WHERE ShipDate IS NOT NULL
-	GROUP BY CustomerID)
-SELECT AVG(CASE WHEN Orders >= 2 THEN 1.0 ELSE 0.0 END) AS RepeatRate
-FROM x;
