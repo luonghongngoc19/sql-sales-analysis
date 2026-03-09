d44191d6e8d1d7fd7c22e7e29a88c759ad3db821
Luong Hong Ngoc <ngocleo2001@gmail.com>
2026-03-09 14:59:11 +0700

    Delete 02_top_products_by_revenue.sql

--- 02_top_products_by_revenue.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-	TOP 10 p.ProductID,  
-	ROUND(SUM(d.LineTotal),0) AS Revenue
-FROM Sales.SalesOrderDetail d
-JOIN Production.Product p ON p.ProductID = d.ProductID
-JOIN Sales.SalesOrderHeader h ON h.SalesOrderID = d.SalesOrderID
-WHERE h.ShipDate IS NOT NULL
-GROUP BY p.ProductID, p.Name
-ORDER BY Revenue DESC;
